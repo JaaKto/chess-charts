131120a4c65631970de88ada4306512a514d0318
Jakub Jablecki <33008680+JaaKto@users.noreply.github.com>
2021-02-28 23:18:43 +0100

    normalize styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import App from "./components/App"
+import * as S from "./index.styles"
 import reportWebVitals from "./reportWebVitals"
 
 ReactDOM.render(
   <React.StrictMode>
+    <S.GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById("root"),
