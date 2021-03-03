4232e7a2a0cfb825116e5e828ec0a171d0805fbd
Jakub Jablecki <33008680+JaaKto@users.noreply.github.com>
2021-03-03 23:40:05 +0100

    fetchData added

---
+++ src/common/utils/baseUrls.ts
@@ -0,0 +1,1 @@
+export type ConcatString = (val: string) => string
---
+++ src/common/utils/fetchData.ts
@@ -0,0 +1,26 @@
+type ObjectMap = { [key: string]: unknown }
+
+interface Options {
+  method: "GET" | "POST" | "PUT" | "DELETE" | "PATCH"
+  headers?: Headers
+  body?: ObjectMap
+}
+
+const handleError = (res: Response) => {
+  if (!res.ok) {
+    throw new Error(res.statusText)
+  }
+  return res
+}
+
+export const fetchData = <T>(
+  endpoint: string,
+  options = {} as Options,
+): Promise<T> =>
+  fetch(endpoint, {
+    method: options.method || "GET",
+    headers: options.headers,
+    body: JSON.stringify(options.body),
+  })
+    .then(handleError)
+    .then((res) => res.json())
