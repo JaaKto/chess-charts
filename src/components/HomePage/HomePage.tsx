7df3804aa4b0677f7233e3f1d5bb2c64d03cfdf3
kokosanki <os_mo@wp.pl>
2021-03-08 00:14:08 +0100

    alt text

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -5,7 +5,7 @@ export const HomePage: React.FC = () => {
   return (
     <S.HomePage>
       <S.HeaderContainer>
-        <S.ChessPiece src="../assets/chess.png" alt="" />
+        <S.ChessPiece src="../assets/chess.png" alt="chess-piece" />
         <S.HeaderText>Chess-charts</S.HeaderText>
       </S.HeaderContainer>
     </S.HomePage>
