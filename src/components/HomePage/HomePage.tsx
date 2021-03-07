3ea21313f9cc9b3286055b2af057a94ec3feee1b
Jakub Jablecki <33008680+JaaKto@users.noreply.github.com>
2021-03-07 17:25:50 +0100

    Added baseUrl & updated imports

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,12 +1,10 @@
 import React from "react"
 import * as S from "./HomePage.styles"
 
-const HomePage: React.FC = () => {
+export const HomePage: React.FC = () => {
   return (
     <S.HomePage>
       <S.ChessPiece src="../assets/chess.png" alt="" />
     </S.HomePage>
   )
 }
-
-export default HomePage
