76e0bfefe79872838995a4c42ac9e243dae9bd27
Jakub Jablecki <33008680+JaaKto@users.noreply.github.com>
2021-03-11 23:49:11 +0100

    UseD3 hook added

---
+++ src/common/hooks/useD3.ts
@@ -0,0 +1,17 @@
+import React, { RefObject, useRef } from "react"
+import { select, Selection } from "d3"
+
+export const useD3 = (
+  renderChartFn: (
+    node: Selection<SVGSVGElement | null, unknown, null, undefined>,
+  ) => void,
+  dependencies: unknown[],
+): RefObject<SVGSVGElement> => {
+  const ref = useRef<SVGSVGElement>(null)
+
+  React.useEffect(() => {
+    renderChartFn(select(ref.current))
+    return
+  }, dependencies)
+  return ref
+}
