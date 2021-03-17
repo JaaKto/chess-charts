7feabfbd281500414d580da8feb6fc6a9c173dd8
Jakub Jablecki <33008680+JaaKto@users.noreply.github.com>
2021-03-17 23:50:17 +0100

    Adjusted fetchData & fixed request blocker

--- src/common/utils/fetchData.ts
+++ src/common/utils/fetchData.ts
@@ -1,11 +1,3 @@
-type ObjectMap = { [key: string]: unknown }
-
-interface Options {
-  method: "GET" | "POST" | "PUT" | "DELETE" | "PATCH"
-  headers?: Headers
-  body?: ObjectMap
-}
-
 const handleError = (res: Response) => {
   if (!res.ok) {
     throw new Error(res.statusText)
@@ -19,28 +11,27 @@ const addMinutes = (date: Date, minutes: number): Date => {
 
 const handleTooManyRequest = (res: Response) => {
   if (res.status === 429) {
-    const requestTime = addMinutes(new Date(), 1).toDateString()
+    const requestTime = addMinutes(new Date(), 1).toString()
     localStorage.setItem("block-request-time", requestTime)
     throw new Error(res.statusText)
   }
   localStorage.removeItem("block-request-time")
   return res
 }
-const isRequestBlock = (): boolean => {
-  const blockRequestTime = localStorage.getItem("block-request-time") || ""
-  return new Date().getTime() - new Date(blockRequestTime).getTime() > 0
-}
 
-export const fetchData = <T>(
-  endpoint: string,
-  options = {} as Options,
-): boolean | Promise<T> =>
-  !isRequestBlock &&
-  fetch(endpoint, {
-    method: options.method || "GET",
-    headers: options.headers,
-    body: JSON.stringify(options.body),
-  })
-    .then(handleError)
-    .then(handleTooManyRequest)
-    .then((res) => res.json())
+const isRequestBlock: () => boolean = () =>
+  !!localStorage.getItem("block-request-time") &&
+  new Date() < new Date(localStorage.getItem("block-request-time") || "")
+
+export const fetchData = <T>(endpoint: string): Promise<T | string> => {
+  if (isRequestBlock()) {
+    return new Promise<string>((resolve) => resolve("requestBloker")).then(
+      (res) => res,
+    )
+  } else {
+    return fetch(endpoint, { method: "GET" })
+      .then(handleError)
+      .then(handleTooManyRequest)
+      .then((res) => res.json())
+  }
+}
