8b63f4f126c5443813482b127eca34517574aac7
kokosanki <os_mo@wp.pl>
2021-05-05 22:20:44 +0200

    create global style

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,14 @@
 import React from "react"
 import { HomePage } from "components/HomePage"
+import GlobalStyle from "globalStyle"
 
 const App: React.FC = () => {
-  return <HomePage />
+  return (
+    <React.Fragment>
+      <GlobalStyle />
+      <HomePage />
+    </React.Fragment>
+  )
 }
 
 export default App
